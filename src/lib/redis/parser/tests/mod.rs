1e9629a2f71100b488c402fcf45521240c3f90c8
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2019-09-29 18:12:16 +0200

    Add missing test for parser in error case

--- src/lib/redis/parser/tests/mod.rs
+++ src/lib/redis/parser/tests/mod.rs
@@ -82,6 +82,29 @@ fn read_strict_string_type_unknow() {
 }
 
 #[test]
+fn read_strict_string_error() {
+    let stream = TestRedisStream::new(vec![
+        REDIS_TYPE_ERROR,
+        'H' as u8,
+        'e' as u8,
+        'l' as u8,
+        'l' as u8,
+        'o' as u8,
+        '\r' as u8,
+        '\n' as u8,
+    ]);
+    let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
+
+    match read_strict_string(&mut box_stream) {
+        Ok(_) => panic!("Must be return error!"),
+        Err(e) => {
+            assert_eq!(e.kind(), ErrorKind::OtherError);
+            assert_eq!(e.message(), "Hello");
+        }
+    }
+}
+
+#[test]
 fn read_integer_ok() -> Result<(), RedisError> {
     let stream = TestRedisStream::new(vec![
         REDIS_TYPE_INTEGER,
@@ -125,11 +148,11 @@ fn read_integer_bad_type() {
     ]);
     let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
 
-    match read_strict_string(&mut box_stream) {
+    match read_integer(&mut box_stream) {
         Ok(_) => panic!("Must be return error!"),
         Err(e) => {
             assert_eq!(e.kind(), ErrorKind::OtherError);
-            assert_eq!(e.message(), String::from("Not a string but a Array"));
+            assert_eq!(e.message(), String::from("Not an integer but a Array"));
         }
     }
 }
@@ -141,14 +164,41 @@ fn read_integer_type_unknow() {
     ]);
     let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
 
-    match read_strict_string(&mut box_stream) {
+    match read_integer(&mut box_stream) {
         Ok(_) => panic!("Must be return error!"),
         Err(e) => {
             assert_eq!(e.kind(), ErrorKind::OtherError);
             assert_eq!(
                 e.message(),
-                String::from("Not a string but a Unknow '0x46'")
+                String::from("Not an integer but a Unknow '0x46'")
             );
+        }
+    }
+}
+
+#[test]
+fn read_integer_error() {
+    let stream = TestRedisStream::new(vec![
+        REDIS_TYPE_ERROR,
+        'E' as u8,
+        'R' as u8,
+        'R' as u8,
+        ' ' as u8,
+        'H' as u8,
+        'e' as u8,
+        'l' as u8,
+        'l' as u8,
+        'o' as u8,
+        '\r' as u8,
+        '\n' as u8,
+    ]);
+    let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
+
+    match read_integer(&mut box_stream) {
+        Ok(_) => panic!("Must be return error!"),
+        Err(e) => {
+            assert_eq!(e.kind(), ErrorKind::ResponseError);
+            assert_eq!(e.message(), " Hello");
         }
     }
 }
@@ -235,6 +285,39 @@ fn read_bulk_string_type_unknow() {
 }
 
 #[test]
+fn read_bulk_string_error() {
+    let stream = TestRedisStream::new(vec![
+        REDIS_TYPE_ERROR,
+        'E' as u8,
+        'X' as u8,
+        'E' as u8,
+        'C' as u8,
+        'A' as u8,
+        'B' as u8,
+        'O' as u8,
+        'R' as u8,
+        'T' as u8,
+        ' ' as u8,
+        'H' as u8,
+        'e' as u8,
+        'l' as u8,
+        'l' as u8,
+        'o' as u8,
+        '\r' as u8,
+        '\n' as u8,
+    ]);
+    let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
+
+    match read_bulk_string(&mut box_stream) {
+        Ok(_) => panic!("Must be return error!"),
+        Err(e) => {
+            assert_eq!(e.kind(), ErrorKind::ExecAbortError);
+            assert_eq!(e.message(), " Hello");
+        }
+    }
+}
+
+#[test]
 fn read_array_ok() -> Result<(), RedisError> {
     let stream = TestRedisStream::new(vec![
         REDIS_TYPE_ARRAY,
@@ -305,7 +388,7 @@ fn read_array_type() {
 }
 
 #[test]
-fn read_bulk_array_type_unknow() {
+fn read_array_type_unknow() {
     let stream = TestRedisStream::new(vec![
         '.' as u8, 'H' as u8, 'e' as u8, 'l' as u8, 'l' as u8, 'o' as u8, '\r' as u8, '\n' as u8,
     ]);
@@ -319,6 +402,69 @@ fn read_bulk_array_type_unknow() {
                 e.message(),
                 String::from("Not an array but a Unknow '0x46'")
             );
+        }
+    }
+}
+
+#[test]
+fn read_array_error() {
+    let stream = TestRedisStream::new(vec![
+        REDIS_TYPE_ERROR,
+        'L' as u8,
+        'O' as u8,
+        'A' as u8,
+        'D' as u8,
+        'I' as u8,
+        'N' as u8,
+        'G' as u8,
+        ' ' as u8,
+        'H' as u8,
+        'e' as u8,
+        'l' as u8,
+        'l' as u8,
+        'o' as u8,
+        '\r' as u8,
+        '\n' as u8,
+    ]);
+    let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
+
+    match read_array(&mut box_stream) {
+        Ok(_) => panic!("Must be return error!"),
+        Err(e) => {
+            assert_eq!(e.kind(), ErrorKind::BusyLoadingError);
+            assert_eq!(e.message(), " Hello");
+        }
+    }
+}
+
+#[test]
+fn read_array_error2() {
+    let stream = TestRedisStream::new(vec![
+        REDIS_TYPE_ERROR,
+        'N' as u8,
+        'O' as u8,
+        'S' as u8,
+        'C' as u8,
+        'R' as u8,
+        'I' as u8,
+        'P' as u8,
+        'T' as u8,
+        ' ' as u8,
+        'H' as u8,
+        'e' as u8,
+        'l' as u8,
+        'l' as u8,
+        'o' as u8,
+        '\r' as u8,
+        '\n' as u8,
+    ]);
+    let mut box_stream: Box<dyn RedisStream> = Box::new(stream);
+
+    match read_array(&mut box_stream) {
+        Ok(_) => panic!("Must be return error!"),
+        Err(e) => {
+            assert_eq!(e.kind(), ErrorKind::NoScriptError);
+            assert_eq!(e.message(), " Hello");
         }
     }
 }
